82ab3f14fe8526f3963e7a67b1752dd492476e4a
the-dimensionless <sumit.sssingh.singh45@gmail.com>
2020-09-28 15:23:38 +0530

    functional HOC

--- components/withBottomSheet.js
+++ components/withBottomSheet.js
@@ -1,40 +1,42 @@
-import React, { Component } from 'react';
+import React, { useRef } from 'react';
 import { View, ScrollView, StyleSheet, Dimensions, TouchableHighlight } from 'react-native';
 import RBSheet from "react-native-raw-bottom-sheet";
 
-const { height, width } = Dimensions.get('window');
+import { KeyboardAwareScrollView } from 'react-native-keyboard-aware-scrollview';
+
+
+const { height } = Dimensions.get('window');
 
 const withBottomSheet = (SheetViewComponent) => {
-    class BottomSheetView extends Component {
-        componentDidMount = () => {
-        }
-        render = () => {
-            return (
-                <>
-                    <RBSheet
-                        ref={ref => {
-                            this.RBSheet = ref;
-                        }}
-                        height={height / 1.3}
-                        minClosingHeight={20}
+    const BottomSheetView = () => {
+        const refRBSheet = useRef();
+        return (
+            <>
+                <RBSheet
+                    ref={refRBSheet}
+                    height={height / 1.3}
+                    minClosingHeight={20}
 
-                        duration={250}
-                        customStyles={customStyles}
-                        closeOnDragDown={true}
-                        dragFromTopOnly={true}
+                    duration={250}
+                    customStyles={customStyles}
+                    closeOnDragDown={true}
+                    dragFromTopOnly={true}
+                >
+                    <KeyboardAwareScrollView
+                        enableOnAndroid={true}
                     >
-                        <ScrollView>
-                            <SheetViewComponent />
-                        </ScrollView>
-                    </RBSheet>
-                    <TouchableHighlight underlayColor="#DDDDDD" style={styles.slider} onPress={(e) => {
-                        this.RBSheet.open();
-                    }}>
-                        <View style={styles.drag} />
-                    </TouchableHighlight>
-                </>
-            )
-        }
+                        <SheetViewComponent />
+                    </KeyboardAwareScrollView>
+
+                </RBSheet>
+
+                <TouchableHighlight underlayColor="#DDDDDD" style={styles.slider} onPress={(e) => {
+                    refRBSheet.current.open();
+                }}>
+                    <View style={styles.drag} />
+                </TouchableHighlight>
+            </>
+        )
     }
     return BottomSheetView
 };
